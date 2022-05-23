7e3b78b620f66d6af6d85d0984251b27a592320d
virtual-luna <maya.ellington@berkeley.edu>
2022-05-23 09:56:05 -0700

    updated user model with sun tokens

--- models/user.js
+++ models/user.js
@@ -9,6 +9,7 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true, lowercase: true, unique: true },
     password: String,
     photoUrl: String, // string from aws!
+    sunToken: String
   },
   {
     timestamps: true,
