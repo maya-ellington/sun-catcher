655c43d98402a11bc627e800ebfdb76d453571c5
virtual-luna <maya.ellington@berkeley.edu>
2022-05-23 11:35:34 -0700

    set up sunPosts route

---
+++ routes/api/sunPosts.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const postsCtrl = require('../../controllers/sunPosts');
+const multer  = require('multer')
+const upload = multer(); // <- handles multipart/formdata requests(photos)
+// /*---------- Public Routes ----------*/
+router.post('/', upload.single('photo'), postsCtrl.create);
+router.get('/', isLoggedIn, postsCtrl.index)
+
+
+/*---------- Protected Routes ----------*/
+
+function isLoggedIn(req, res, next){
+    if(req.user) next();
+    res.status(401).json({data: 'not authorized! Please log in'})
+}
+
