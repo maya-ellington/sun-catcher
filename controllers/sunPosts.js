ab79b7ded9c0529146861c5c76bf6806b8bdf574
virtual-luna <maya.ellington@berkeley.edu>
2022-05-23 11:50:33 -0700

    set up sunPosts controller

---
+++ controllers/sunPosts.js
@@ -0,0 +1,45 @@
+const SunPost = require('../models/sunPost');
+const S3 = require('aws-sdk/clients/s3');
+const { v4: uuidv4 } = require('uuid');
+
+const s3 = new S3();
+
+module.exports = {
+    create,
+    index,
+    // delete
+}
+
+function create(req, res){
+    console.log(req.file, req.body, 'this is create method', req.user)
+    try {
+        const filePath = `${uuidv4()}/${req.file.originalname}`
+        const params = {Bucket: process.env.BUCKET_NAME, Key: filePath, Body: req.file.buffer};
+        s3.upload(params, async function(err, data){
+			console.log(err, ' from aws')
+            const post = await SunPost.create({location: req.body.location, date: req.body.date, sun_watchers: req.body.sun_watchers, description: req.body.description, sunQuote: req.body.sunQuote, postType: req.body.postType, user: req.user, photoUrl: data.Location});
+            console.log(post)
+
+            await post.populate('user');
+		
+            res.status(201).json({post: post})
+        })
+
+
+    } catch(err){
+        console.log(err)
+        res.json({data: err})
+    }
+}
+
+async function index(req, res){
+    try {
+        // this populates the user when you find the posts
+        // so you'll have access to the users information 
+        // when you fetch teh posts
+        const posts = await SunPost.find({}).populate('user').exec()
+        res.status(200).json({posts})
+    } catch(err){
+
+    }
+}
