26cb1f80363c8f1da88702d93bf85b8af45dd640
virtual-luna <maya.ellington@berkeley.edu>
2022-05-23 11:36:47 -0700

    set up comment route

---
+++ routes/api/comments.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const commentsCtrl = require('../../controllers/comments')
+
+router.post('/posts/:id/comments', commentsCtrl.create)
+router.delete('/comments/:id', commentsCtrl.deleteComment)
+
+module.exports = router;
