c0eb92fac511d4149035d912450010f6489c7a93
virtual-luna <maya.ellington@berkeley.edu>
2022-05-27 03:08:16 -0700

    added username to feed page ;

--- src/components/SunPostCard/SunPostCard.jsx
+++ src/components/SunPostCard/SunPostCard.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Card, Image } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
 
 export default function SunPostCard({ removeSunPost, sunPost, isProfile }){
     
@@ -23,6 +25,20 @@ export default function SunPostCard({ removeSunPost, sunPost, isProfile }){
         <Card key={sunPost._id} raised>
           <Image src={`${sunPost.photoUrl}`} wrapped ui={false} />
           <Card.Content>
+          <Card.Header>
+            <Link to={`/${sunPost.user.username}`}>
+              <Image
+                size="large"
+                avatar
+                src={
+                  sunPost.user.photoUrl
+                    ? sunPost.user.photoUrl
+                    : "https://react.semantic-ui.com/images/wireframe/square-image.png"
+                }
+              />
+              {sunPost.user.username}
+            </Link>
+          </Card.Header>
             <Card.Description>{sunPost.location}</Card.Description>
             <Card.Description>{sunPost.date}</Card.Description>
             <Card.Description>{sunPost.postType}</Card.Description>
